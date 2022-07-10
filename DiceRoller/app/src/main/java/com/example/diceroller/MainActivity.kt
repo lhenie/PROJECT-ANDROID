340585f13db2fc4a48dc9bc156cc5ed9b7e378bc
Roseleny Oligo <roselenyoligo@gmail.com>
2022-07-10 09:49:13 +0800

    Commit on July 10, 2022 9:49 am

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -28,9 +28,12 @@ class MainActivity : AppCompatActivity() {
         // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        val dice2 = Dice(6)
+        val diceRoll2 = dice2.roll()
 
         // Find the ImageView in the layout
         val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
 
         // Determine which drawable resource ID to use based on the dice roll
         val drawableResource = when (diceRoll) {
@@ -41,12 +44,22 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
+        val drawableResource2 = when (diceRoll2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
         // Update the ImageView with the correct drawable resource ID
         diceImage.setImageResource(drawableResource)
+        diceImage2.setImageResource(drawableResource2)
 
         // Update the content description
         diceImage.contentDescription = diceRoll.toString()
+        diceImage2.contentDescription = diceRoll2.toString()
     }
 }
 
